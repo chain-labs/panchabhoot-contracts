84704d88d44a8cac77370978a9f18c7379a3226f
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2022-11-11 20:19:50 +1100

    feat: setup repository complete

---
+++ hardhat.config.ts
@@ -0,0 +1,105 @@
+import { checkForUndefined } from './utils/checkers';
+import dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+import { NetworkUserConfig } from "hardhat/types";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-packager";
+import "hardhat-tracer";
+import "hardhat-deploy";
+dotenv.config({ path: "./.env" });
+
+const { INFURA_KEY, ETHERSCAN_KEY, PRIVATE_KEY, MNEMONIC } = process.env;
+
+checkForUndefined("INFURA_KEY", INFURA_KEY);
+checkForUndefined("ETHERSCAN_KEY", ETHERSCAN_KEY);
+checkForUndefined("PRIVATE_KEY", PRIVATE_KEY);
+checkForUndefined("MNEMONIC", MNEMONIC);
+
+const OPTIMIZER_RUNS = 1000;
+
+const sharedNetworkConfig: NetworkUserConfig = {};
+
+const sharedCompilerConfig = {
+  optimizer: {
+    enabled: true,
+    runs: OPTIMIZER_RUNS,
+  },
+  outputSelection: {
+    "*": {
+      "*": ["storageLayout"],
+    },
+  },
+};
+
+// Order of priority for account/signer generation:
+// 1) .env/PRIVATE_KEY
+// 2) .env/MNEMONIC
+// 3) ./mnemonic.txt
+if (PRIVATE_KEY) {
+  sharedNetworkConfig.accounts = [PRIVATE_KEY];
+} else if (MNEMONIC) {
+  sharedNetworkConfig.accounts = {
+    mnemonic: MNEMONIC,
+  };
+} else {
+  throw Error("No Private Key or Mnemonic supplied");
+}
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  paths: {
+    artifacts: "build/artifacts",
+    cache: "build/cache",
+    deploy: "deploy",
+    sources: "contracts",
+  },
+
+  networks: {
+    ethereum: {
+      ...sharedNetworkConfig,
+      url: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+    },
+    goerli: {
+      ...sharedNetworkConfig,
+      url: `https://goerli.infura.io/v3/${INFURA_KEY}`,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+        settings: sharedCompilerConfig,
+      },
+    ],
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    coinmarketcap: "",
+    excludeContracts: ["contracts/testing/"],
+  },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: true,
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+  etherscan: {
+    apiKey: {
+      ethereum: ETHERSCAN_KEY,
+      goerli: ETHERSCAN_KEY,
+    },
+  },
+  external: {
+    contracts: [
+      {
+        artifacts: "./imports/artifacts",
+      },
+    ],
+  },
+};
+
+export default config;
