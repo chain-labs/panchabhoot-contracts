9ebce317f5614264d5d083829e1748ea4ea84770
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2022-11-15 16:14:54 +1100

    feat: created controller contract and added test for controller

---
+++ test/Constants.ts
@@ -0,0 +1,7 @@
+import { ContractNames } from "../types/ContractNames";
+
+export const contractsName: ContractNames = {
+  CONTROLLER: "Controller",
+};
+
+export const UNIT_TEST = "Unit Test: ";
---
+++ test/ERROR_STRINGS.ts
@@ -0,0 +1,6 @@
+// Error codes of @openzeppelin/contracts/access/Ownable.sol
+export const OWNABLE_NOT_OWNER = "Ownable: caller is not the owner";
+
+// error codes of @openzeppelin/contracts/*/Initializable
+export const INITIALIZABLE_ALREADY_INITIALIZED =
+  "Initializable: contract is already initialized";
