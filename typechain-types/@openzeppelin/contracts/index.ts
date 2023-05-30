a4567b15958d92ba9a3c12fa578dd971bcfa64e9
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2023-05-30 16:22:14 +0530

    chore: deployed contracts to goerli and verified them

--- typechain-types/@openzeppelin/contracts/index.ts
+++ typechain-types/@openzeppelin/contracts/index.ts
@@ -3,7 +3,3 @@
 /* eslint-disable */
 import type * as interfaces from "./interfaces";
 export type { interfaces };
-import type * as token from "./token";
-export type { token };
-import type * as utils from "./utils";
-export type { utils };
--- typechain-types/factories/@openzeppelin/contracts/index.ts
+++ typechain-types/factories/@openzeppelin/contracts/index.ts
@@ -2,5 +2,3 @@
 /* tslint:disable */
 /* eslint-disable */
 export * as interfaces from "./interfaces";
-export * as token from "./token";
-export * as utils from "./utils";
